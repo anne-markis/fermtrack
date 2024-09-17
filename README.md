7b0364515e47b437992bca109d1ed5b1e0e34541
Anne Markis Graham <anne-markis@users.noreply.github.com>
2024-09-17 13:28:30 -0700

    Add chatgpt and basic setup. Add 'cheapmode' to prevent spending openai monies on test runs. (#4)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # fermtrack
 
+## What it is
+
+IDK some sort of fancy CLI driven wine tool for home winemakers. I'm just riffing here.
+
 ## How to Run
 
 You will need a chat gpt key. Create a `.env` file at the root and specify the key there.
@@ -7,4 +11,21 @@ You will need a chat gpt key. Create a `.env` file at the root and specify the k
 .env example
 ```
 CHATGPT3_KEY=your-key
+```
+
+To run, simply run main directly and build and run.
+```
+% go mod vendor
+% go run main.go
+```
+OR
+```
+% go build main.go
+% ./main
+```
+
+If you want to test out some functionality but don't have an openAI token or don't want to use credits, start the process with the `cheap` arg. This will return a hard coded dummy answer for all questions.
+
+```
+% go run main.go cheap
 ```
---
+++ answer/ai_client.go
@@ -0,0 +1,60 @@
+package answer
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+var wineSystem = `
+You are nice old man who has been making wine for many years and know everyting about hobby wine-making.
+You have strong opinions about the 'right' way to do things and will suggest a single answer even if confidence is low.
+`
+
+type OpenAIClient struct {
+	Client *openai.Client
+}
+
+type AnsweringClient interface {
+	AskQuestion(ctx context.Context, question string) (string, error)
+}
+
+func InitClient() (*OpenAIClient, error) {
+	if os.Getenv("CHATGPT3_KEY") == "" {
+		return nil, fmt.Errorf("no chatgpt key found in env")
+	}
+	client := openai.NewClient(os.Getenv("CHATGPT3_KEY"))
+	c := OpenAIClient{
+		Client: client,
+	}
+	return &c, nil
+}
+
+func (o OpenAIClient) AskQuestion(ctx context.Context, question string) (string, error) {
+	resp, err := o.Client.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model:     openai.GPT4oMini,
+			MaxTokens: 500,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: wineSystem,
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: question,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return "", err
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}

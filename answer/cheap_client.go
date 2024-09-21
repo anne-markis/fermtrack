bcfc8fcce951ca8721ffd02f19480837ba012f72
Anne Markis Graham <anne-markis@users.noreply.github.com>
2024-09-21 13:06:55 -0700

    Misc updates (#9)

--- answer/cheap_client.go
+++ answer/cheap_client.go
@@ -12,8 +12,19 @@ type CheapClient struct {
 func (o CheapClient) AskQuestion(ctx context.Context, question string) (string, error) {
 	question = strings.Join(strings.Fields(question), "")
 	if question == "" {
-		return "Ask the wine wizard anything you like.", nil
+		return "Ask me, the wine wizard, anything you like.", nil
 	}
 	time.Sleep(1 * time.Second) // simulate slow AI answer
-	return "Sorry I'm too drunk for that", nil
+	return `My apologies, but at the moment, my thoughts seem to be a bit hazy, much like a foggy morning.
+
+It's as if my mind is a glass of wine, swirling with ideas, but not quite focused enough to give you a clear answer.
+
+Perhaps we could revisit this later when my mind is a bit more sober and my thoughts are clearer.
+
+Enjoy this poem instead:
+
+Fermenting mind swirls,
+Grape juice turned to liquid fire,
+Drunkard dreams of wine.
+	`, nil
 }
